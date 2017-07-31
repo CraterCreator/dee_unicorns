16ce28707275714b3b49c6784be90f4d59a75ce3
CratorCreator <jordanfischer13@gmail.com>
2017-07-31 16:00:22 +1000

    updated

--- dee_unicorns/Assets/Scripts/GameManager.cs
+++ dee_unicorns/Assets/Scripts/GameManager.cs
@@ -2,36 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using System.Net;
+using System.Net.Security;
+using System.Net.Mail;
+using System.Security.Cryptography.X509Certificates;
 
 public class GameManager : MonoBehaviour
 {
-    public bool showLog;
-    public GameObject login, create;
+    public bool showLog, accForgot;
+    public GameObject login, create, forgot;
     public string username, password, email;
+    public InputField Username, Email, Password, forgotEmail; 
     // Use this for initialization
     void Start()
     {
         showLog = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!showLog)
+        if(accForgot)
         {
-            login.SetActive(false);
-            create.SetActive(true);
+            if (forgotEmail.text != email)
+            {
+                email = forgotEmail.text;
+            }
         }
-        else
+     
+        if (!showLog && !accForgot)
         {
-            login.SetActive(true);
-            create.SetActive(false);
+            if (Username.text != username)
+            {
+                username = Username.text;
+            }
+
+            if (Password.text != password)
+            {
+                password = Password.text;
+            }
+
+            if (Email.text != email)
+            {
+                email = Email.text;
+            }
         }
     }
 
     public void Press()
     {
-        showLog = !showLog;
+        login.SetActive(false);
+        create.SetActive(true);
+    }
+    public void Press2()
+    {
+        login.SetActive(false);
+        forgot.SetActive(true);
     }
 
     public void CreateAccountButton()
@@ -41,7 +69,7 @@ public class GameManager : MonoBehaviour
 
     IEnumerator CreateUser(string userName, string passWord, string eMail)
     {
-        string createUserURL = "http://localhost/unicorns_dee/InsertUser.php";
+        string createUserURL = "localhost/unicorns_dee/InsertUser.php";
 
         WWWForm userInfo = new WWWForm();
         userInfo.AddField("usernamePost", userName);
@@ -53,5 +81,45 @@ public class GameManager : MonoBehaviour
 
         Debug.Log(www.text);
 
+    }
+
+    public void StartLogin()
+    {
+        StartCoroutine(Login(username, password));
+    }
+
+    public void ForgotAccount()
+    {
+        MailMessage mail = new MailMessage();
+
+        mail.From = new MailAddress("sqlunityclasssydney@gmail.com");
+        mail.To.Add(email);
+        mail.Subject = "Password Reset";
+        mail.Body = "Hello User /n/n Someones gone and done a goof /n Reset password here...";
+
+        SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+        smtpServer.Port = 25;
+        smtpServer.Credentials = new NetworkCredential("sqlunityclasssydney@gmail.com", "sqlpassword") as ICredentialsByHost;
+        smtpServer.EnableSsl = true;
+        ServicePointManager.ServerCertificateValidationCallback = delegate (object s, X509Certificate cert, X509Chain chain, SslPolicyErrors errors) { return true; };
+        smtpServer.Send(mail);
+
+        Debug.Log("Email Sent");
+    }
+
+    IEnumerator Login(string username, string password)
+    {
+        string loginURL = "localhost/unicorns_dee/Login.php";
+        WWWForm loginForm = new WWWForm();
+        loginForm.AddField("usernamePost", username);
+        loginForm.AddField("passwordPost", password);
+        WWW www = new WWW(loginURL, loginForm);
+        yield return www;
+
+        Debug.Log(www.text);
+        if(www.text == "Login Success")
+        {
+            // Action
+        }
     }
 }
