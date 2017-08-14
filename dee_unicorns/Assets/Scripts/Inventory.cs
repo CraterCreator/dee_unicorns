bd138b7e19190341ffd5e79e10ecae9de5691111
CratorCreator <jordanfischer13@gmail.com>
2017-08-14 15:59:21 +1000

    Characters

---
+++ dee_unicorns/Assets/Scripts/Inventory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public string[] items;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(LoadItemData());
+    }
+
+    IEnumerator LoadItemData()
+    {
+        WWW itemsUrl = new WWW("localhost/unicorns_dee/Itemdata.php");
+        yield return itemsUrl;
+        string itemData = itemsUrl.text;
+        items = itemData.Split(';');
+    }
+
+}
